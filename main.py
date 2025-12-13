f3fe7111d55fa82248d6b317955ea6ad46051204
Cyberis <christopher.bortz@gmail.com>
2025-12-13 02:27:18 -0500

    Add --Verbose option

--- main.py
+++ main.py
@@ -17,18 +17,21 @@ def main():
     # Get a prompt from the user
     parser = argparse.ArgumentParser(description="Cyberis AI Assistant")
     parser.add_argument('user_prompt', type=str, help='How can I assist you today?')
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    print(f'User prompt: {args.user_prompt}')
     
     # Generate content using the Gemini model
     response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     
-    # Print token usage
-    if not response.usage_metadata:
-        raise RuntimeError("No usage metadata found in the response.")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # If The User Wants Verbose output print that here
+    if args.verbose:
+        if not response.usage_metadata:
+            raise RuntimeError("No usage metadata found in the response.")
+        print(f'User prompt: {args.user_prompt}')
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
     # Print the response
     print(f"Response:\n{response.text}")
     
