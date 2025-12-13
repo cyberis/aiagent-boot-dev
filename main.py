2faf3f57ebc2ea179ff514a2ac37e1507d914368
Cyberis <christopher.bortz@gmail.com>
2025-12-13 01:46:32 -0500

    Return Token Counts

--- main.py
+++ main.py
@@ -14,8 +14,16 @@ def main():
     
     # Example usage of the client
     content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    print(f'User prompt: {content}')
     response = client.models.generate_content(model="gemini-2.5-flash", contents=content)
-    print(response.text)
+    
+    # Print token usage
+    if not response.usage_metadata:
+        raise RuntimeError("No usage metadata found in the response.")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # Print the response
+    print(f"Response:\n{response.text}")
     
 
 if __name__ == "__main__":
